fb5432265967c4af060f8403341a7026188166eb
Vadzim Yakushau <vadzim_yakushau@epam.com>
2019-02-11 14:22:08 +0100

    Add OfflineModal to Product

--- App/screens/Product.js
+++ App/screens/Product.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { connect } from "react-redux";
 import styles from "../theme/ApplicationStyles";
+import OfflineModal from '../components/Offline';
 
 class Product extends React.Component {
 
@@ -10,7 +11,7 @@ class Product extends React.Component {
     return (
 
       <View style={styles.container}>
-
+        <OfflineModal/>
         {error ? <Text>Error! {error.message}</Text> : null}
         {
         loading ? <Text>Loading!</Text> :
