abd6ae784fcb317abd746dc91aa433b4b860a358
Vadzim Yakushau <vadzim_yakushau@epam.com>
2019-02-11 14:17:18 +0100

    Remove trash out

--- App/router.js
+++ App/router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import { FontAwesome } from 'react-native-vector-icons';
-// import { OfflineModal } from './components/Offline';
 import Login from './screens/Login';
 import Main from './screens/Main';
 import Product from './screens/Product';
@@ -55,18 +54,12 @@ export const createRootNavigator = (signedIn = false) => {
         screen: SignedIn,
         navigationOptions: {
           gesturesEnabled: false,
-          header: (
-            {/*<OfflineModal />*/}
-          )
         }
       },
       SignedOut: {
         screen: SignedOut,
         navigationOptions: {
           gesturesEnabled: false,
-          header: (
-            {/*<OfflineModal />*/}
-          )
         }
       },
     },
